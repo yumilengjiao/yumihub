c8a8314de32357a26435875f36900cd107074d01
yumilengjiao <1554517392@qq.com>
2026-01-21 21:05:19 +0800

    背景图像显示

--- src/components/GameList/index.tsx
+++ src/components/GameList/index.tsx
@@ -2,17 +2,16 @@ import { useEffect, useState } from "react"
 import { Card, CardContent } from "../ui/card"
 import { Carousel, CarouselApi, CarouselContent, CarouselItem } from "../ui/carousel"
 import { cn } from "@/lib/utils"
-import { invoke } from '@tauri-apps/api/core';
+import { convertFileSrc, invoke } from '@tauri-apps/api/core';
 import useGameStore from "@/store/gameStore";
-import { GameMeta, GameMetaList } from "@/types/game";
+import { GameMetaList } from "@/types/game";
 
 
 export const GameList = (props: {}) => {
   // ... 在你的组件内部
   const [api, setApi] = useState<CarouselApi>()
   const [currentIndex, setCurrentIndex] = useState<number>(0)
-  const [selectedGame, setSelectedGame] = useState<GameMeta>()
-  const { gameMetaList, setGameMetaList } = useGameStore()
+  const { selectedGame, updateSelectedGame, gameMetaList, setGameMetaList } = useGameStore()
 
 
   //向状态管理系统拿数据
@@ -20,21 +19,16 @@ export const GameList = (props: {}) => {
     try {
       const gameList = await invoke<GameMetaList>('get_game_meta_list_cmd')
       setGameMetaList(gameList)
+
+      updateSelectedGame(gameList[0])
     } catch (err) {
       console.error(err)
     }
   }
-
   useEffect(() => {
     getGamelist()
-    if (!api) return
-    // 监听滚动事件
-    api.on("select", () => {
-      // selectedScrollSnap() 会返回当前对齐在“激活位置”的索引
-      // setCurrentIndex(api.selectedScrollSnap())
-    })
+  }, [])
 
-  }, [api])
   return (
     <div className="overflow-hidden">
       <div className={cn(
@@ -64,18 +58,21 @@ export const GameList = (props: {}) => {
               )}
               onClick={(_) => {
                 setCurrentIndex(index)
-                setSelectedGame(g)
+                updateSelectedGame(g)
                 api?.scrollTo(index, false)
               }}
             >
               <Card className={cn(
-                "bg-amber-950 object-cover border-none",
+                "object-cover border-none",
                 "aspect-165/225  min-w-41.25 min-h-56.25 origin-bottom transition-all duration-300",
                 currentIndex != index && "scale-80"
               )}>
-                <CardContent className=" flex aspect-square items-center justify-center">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
-                </CardContent>
+                <div className=" w-full">
+                  <img
+                    src={convertFileSrc(g.cover)}
+                    className="h-full w-full object-cover"
+                  />
+                </div>
               </Card>
             </CarouselItem>
           ))}
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,16 +1,30 @@
-import clsx from "clsx";
-import './index.css'
+import useGameStore from "@/store/gameStore";
+import { cn } from "@/lib/utils";
+import { convertFileSrc } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
 import { GameList } from "@/components/GameList";
 
 
 export default function Home() {
+  const { selectedGame } = useGameStore()
+  const [url, setUrl] = useState<string>("")
+  useEffect(() => {
+    setUrl(convertFileSrc(selectedGame?.background || "/nihao"))
+    console.log(url)
+  }, [selectedGame])
 
   return (
-    <div className={clsx(
-      'home'
-    )}>
+    <div className={cn(
+      'home bg-cover bg-center'
+    )}
+      style={{
+        // 使用内联样式确保 asset:// 协议能被正确解析
+        backgroundImage: url ? `url("${url}")` : 'none',
+        backgroundColor: '#1a1a1a' // 设置一个深色底色，没图时也不会白屏
+      }}
+    >
       <div className="grid grid-cols-12 gap-4 pl-6 min-0 h-full">
-        <div className="bg-gray-400 col-span-12 flex items-end">
+        <div className="col-span-12 flex items-end" >
           <GameList />
         </div>
       </div>
