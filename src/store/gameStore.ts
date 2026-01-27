a03ff73a6fe1217107df16a6b05c2e97a37b4041
yumilengjiao <1554517392@qq.com>
2026-01-27 21:37:01 +0800

    用户前端仓库

--- src/store/gameStore.ts
+++ src/store/gameStore.ts
@@ -1,5 +1,4 @@
 import { GameMeta, GameMetaList } from '@/types/game'
-import { convertFileSrc } from '@tauri-apps/api/core'
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 
@@ -16,16 +15,31 @@ const useGameStore = create<GameStore>()(
   immer((set) => ({
     selectedGame: null,
     gameMetaList: [],
+
+    /**
+     * 用于更改当前在主页选中的游戏
+     * @param game 新的选中的游戏数据
+     */
     updateSelectedGame: (game) => {
       set((state) => {
         state.selectedGame = game
       })
     },
+
+    /**
+     * 用于初始化时调用初始前端仓库游戏列表数据
+     * @param gameMetaList - 要设置的游戏列表数据
+     */
     setGameMetaList(gameMetaList) {
       set((state) => {
         state.gameMetaList = gameMetaList
       })
     },
+
+    /**
+     * 用于修改一个已经存在的单个游戏的数据
+     * @param game - 要用于修改的游戏数据
+     */
     setGameMeta(game) {
       set((state) => {
         let index = state.gameMetaList.findIndex((g) => g.id = game.id)
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,12 @@
 export interface User {
-  userId: string
-  userName: string
-  totalPlayTime: number
-  libraryPaths: string[]
-  folders: string[]
+  id: string;
+  userName: string;
+  avatar: string;
+  localAvatar: string;
+  gamesCount: number; // Rust i64 对应 JS number
+  favoriteVnId: string;
+  totalPlayTime: number;
+  gamesCompletedNumber: number;
+  lastPlayAt: string | null;
+  createdAt: string | null;
 }
-
